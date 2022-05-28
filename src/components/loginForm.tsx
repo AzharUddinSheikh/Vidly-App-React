f7d7da99055d111dd92901990981a064263f742b
Azhar <Azharthegreat123@gmail.com>
2022-05-28 16:38:38 +0530

    Login structure removed default behavior

--- src/components/loginForm.tsx
+++ src/components/loginForm.tsx
@@ -1,19 +1,37 @@
-import React,  { Component } from 'react';
+import React, { Component } from 'react';
 
 interface LoginFormProps {
-    
+
 }
- 
+
 interface LoginFormState {
-    
+
 }
- 
+
 class LoginForm extends React.Component<LoginFormProps, LoginFormState> {
-    render() { 
+
+    handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+    }
+
+    render() {
         return (
-            <h1>Login</h1>
+            <div>
+                <h1>Login</h1>
+                <form onSubmit={this.handleSubmit}>
+                    <div className="form-group">
+                        <label htmlFor="username">Username</label>
+                        <input id="username" type="text" className="form-control" />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="password">Password</label>
+                        <input id='password' type="text" className="form-control" />
+                    </div>
+                    <button className="btn btn-primary">Login</button>
+                </form>
+            </div>
         );
     }
 }
- 
+
 export default LoginForm;
