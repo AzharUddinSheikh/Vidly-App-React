b5b73d44dffe25b0ccaf91b4d1fd25b6bf3f9274
Azhar <Azharthegreat123@gmail.com>
2022-05-29 15:33:11 +0530

    adding simple validation for username and password field

--- src/components/loginForm.tsx
+++ src/components/loginForm.tsx
@@ -1,27 +1,41 @@
 import React from 'react';
 import Input from './common/input';
 
-interface LoginFormProps {
-}
-
-interface LoginFormState {
-}
 
 interface Account {
-    username: string,
-    password : string,
+    username?: string,
+    password? : string,
 }
 
-class LoginForm extends React.Component<LoginFormProps, LoginFormState> {
+class LoginForm extends React.Component {
 
     state = {
-        account : {username : '', password : ''}
+        account : {username : '', password : ''},
+        errors : {},
     }
 
-    username = React.createRef<HTMLInputElement>();
+    // username = React.createRef<HTMLInputElement>();
+
+    validate = () => {
+        const errors : Account = {};
+        const {account} = this.state;
+        
+        if (account.username.trim() === '') 
+            errors.username = 'Username is required.';
+        if (account.password.trim() === '')
+            errors.password = 'Password is required.';
+        
+            return Object.keys(errors).length === 0 ? null : errors;
+    }
 
     handleSubmit = (e: React.FormEvent) => {
         e.preventDefault();
+
+        const errors = this.validate();
+        this.setState({errors});
+        
+        if (errors) return;
+        console.log("submitted");
     }
 
     handleChange = (e : React.ChangeEvent<HTMLInputElement>) => {
